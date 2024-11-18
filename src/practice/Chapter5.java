ae124b348ab237eade4865128b28f759e63b21a5
NaokiSakamoto <187244142+mizukan390s@users.noreply.github.com>
2024-11-18 21:35:01 +0900

    課題再提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,22 +1,32 @@
 package practice;
 
-import java.time.LocalDate;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Chapter5 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Task tasks = new Task();
-		Map<LocalDate, String> map = new TreeMap<>();
+		// 以下のタスクを生成
+		Task task1 = new Task("2021年10月21日 牛乳を買う。");
+		Task task2 = new Task("2021年9月15日 ○○社面談。");
+		Task task3 = new Task("2021年12月4日 手帳を買う。");
+		Task task4 = new Task("2021年8月10日 散髪に行く。");
+		Task task5 = new Task("2021年11月9日 スクールの課題を解く。");
 
-		for (int i = 0; i < tasks.date.length; i++) {
-			map.put(tasks.date[i], tasks.content[i]);
-		}
+		// リストに格納
+		List<String> list = new ArrayList<>();
+		list.add(task1.getDate() + ":" + task1.getContent());
+		list.add(task2.getDate() + ":" + task2.getContent());
+		list.add(task3.getDate() + ":" + task3.getContent());
+		list.add(task4.getDate() + ":" + task4.getContent());
+		list.add(task5.getDate() + ":" + task5.getContent());
 
-		for (Map.Entry<LocalDate, String> task : map.entrySet()) {
-			System.out.println(task.getKey() + ":" + task.getValue());
+		Collections.sort(list);
+
+		for (String x : list) {
+			System.out.println(x);
 		}
 
 	}
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,23 +1,26 @@
 package practice;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class Task {
 
-public class Task {	
-	
-	LocalDate[] date = {
-			LocalDate.of(2021,10,21),
-			LocalDate.of(2021,9,15),
-			LocalDate.of(2021,12,4),
-			LocalDate.of(2021,8,10),
-			LocalDate.of(2021,11,9)
-			};
-	
-	String[] content = {
-			"牛乳を買う。",
-			"○○社面談。",
-			"手帳を買う。",
-			"散髪に行く。",
-			"スクールの課題を解く。"
-			};
+	private LocalDate date;
+	private String content;
+
+	public Task(String task) {
+		String[] str = task.split(" ");
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年M月d日");
+		date = LocalDate.parse(str[0], f);
+		content = str[1];
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
 }
